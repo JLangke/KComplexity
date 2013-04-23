0636b71f4b9f8d118a0dfe042d2c79445a9918e4
Peter Boothe <peter.boothe@manhattan.edu>
2013-04-23 16:45:02 -0400

    Added a better view to the listRankings.  Took the survey a bit.

--- 3x3survey/listRankings.py
+++ 3x3survey/listRankings.py
@@ -6,17 +6,23 @@ def get(db, key, default):
 	else:
 		return default
 
+def bit(b, n):
+	return "#."[(n >> b) & 1]
+
+def stringify(n):
+	return "\n".join([ "".join([bit(x+3*y, n) for x in range(3)]) for y in range(3) ])
+
 import cgi
 import sys
 
 import gdbm
 db = gdbm.open("rankings.db", 'c')
 data = []
-for i in range(16):
+for i in range(512):
 	mu = float(get(db, str(i) + "-avg", 25))
 	sig = float(get(db, str(i) + "-dev", 8.3333))
 	data.append( (mu, i, sig) )
 
 data.sort()
 for mu, i, sigma in data:
-	print i, mu, sigma
+	print stringify(i), i, mu, sigma
